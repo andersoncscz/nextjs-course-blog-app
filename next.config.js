98896769f0c890bc2f4648f2d4868b32480e333f
Anderson dos Santos Cruz <andersondossantoscruz@Andersons-MacBook-Pro.local>
2021-07-15 11:41:33 -0300

    final version

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_SERVER } = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 module.exports = (phase) => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    // Config with your production environment data
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    // Config with your dev environment data
     return {
       reactStrictMode: true,
       env: {
         mongo_db_username: 'NextJSCourse',
         mongo_db_password: '123mudar',
         mongo_db_clustername: 'nextjscourse',
-        mongo_db_database: 'blog-production',
+        mongo_db_database: 'blog-dev',
       }
     }
   }
@@ -20,7 +20,7 @@ module.exports = (phase) => {
       mongo_db_username: 'NextJSCourse',
       mongo_db_password: '123mudar',
       mongo_db_clustername: 'nextjscourse',
-      mongo_db_database: 'blog-dev',
+      mongo_db_database: 'blog-production',
     }
   }
 }
